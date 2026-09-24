7e1ed15e5e89e77e4603e74450298ac76874205b
LalGaneshMamidi <lalganeshmamidi123@gmail.com>
2026-09-24 15:26:49 +0530

    quick change

--- tests/CustomFixtures.spec.js
+++ tests/CustomFixtures.spec.js
@@ -1,10 +1,13 @@
 const {expect}=require("@playwright/test")
 const {customtest} = require('../utils/Fixtures');
 
+//fixtures demo
+
 customtest('fixtures demo',async({authenticatedPage, createOrder, testDataForOrder})=>{
     await authenticatedPage.goto('https://rahulshettyacademy.com/client/');
     await authenticatedPage.locator("[routerlink='/dashboard/myorders']").click();
     await authenticatedPage.locator("tbody").waitFor();
     await expect(authenticatedPage.getByText(createOrder.orderId)).toBeVisible();
     console.log(testDataForOrder.productName);
+
 })
